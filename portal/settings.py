765c61e2d5282e8c11c52d17c99c5d415248fad2
IlhomJumayevUrish <ilhomjumayev6022@gmail.com>
2021-05-26 18:36:02 +0500

    Two_comment
    Two comment

--- portal/settings.py
+++ portal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'portal.urls'
+TEMPLATE_DIR=os.path.join(BASE_DIR,"template")
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
